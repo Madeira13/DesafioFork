cb6165fba1006d7f80fb284ce2c00f52693fcff8
Swift <swift@fernando.local>
2018-01-15 21:43:42 -0200

    mostra mensagens com toque na tela

--- DesafioFork/ViewController.swift
+++ DesafioFork/ViewController.swift
@@ -9,17 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        let toque = UITapGestureRecognizer(target: self, action: #selector(showMensage))
+        view.addGestureRecognizer(toque)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: Methods
+    func mensage() -> String {
+        let agora = Date()
+        guard let meioDia = Calendar.current.date(bySettingHour: 12, minute: 00, second: 00, of: agora) else { return "" }
+        guard let seisHoras = Calendar.current.date(bySettingHour: 18, minute: 00, second: 00, of: agora) else { return "" }
+        
+        if agora >= meioDia && agora <= seisHoras {
+            return "Boa Tarde.."
+        } else if agora >= seisHoras {
+            return "Boa Noite..."
+        } else {
+            return "Bom Dia..."
+        }
+    }
+    
+    @objc func showMensage() {
+        let alerta = UIAlertController.init(title: nil, message: mensage(), preferredStyle: UIAlertControllerStyle.alert)
+        alerta.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        self.present(alerta, animated: true, completion: nil)
+        
+    }
+    
 }
 
